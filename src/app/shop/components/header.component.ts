54b56ee8bc42f994f3e258b8cd84d94f308c74b8
tranhuunhan1104 <tranhuunhan1104@gmailcom>
2022-12-27 17:23:10 +0700

    cap nhat gio hang

--- src/app/shop/components/header.component.ts
+++ src/app/shop/components/header.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute , Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { environment } from 'src/environments/environment';
 import { ShopService } from '../shop.service';
 
@@ -10,18 +10,40 @@ import { ShopService } from '../shop.service';
 export class HeaderComponent implements OnInit {
   products: any;
   url: string = environment.url;
-  constructor(private shopService: ShopService, private _Router: Router,) { }
-  ngOnInit() {
-
+  customer_id: any;
+  orders: any;
+  totalPrice: number = 0;
+  constructor(private shopService: ShopService,
+    private _Router: Router,
+    private ShopService: ShopService,
+    private route: ActivatedRoute,) { }
+  ngOnInit(): void {
+    this.customer_id = this.route.snapshot.params['id'];
+    this.ShopService.getListOrder(this.customer_id).subscribe(res => {
+      this.orders = res;
+      console.log(res);
+      // for(let orderDetail of this.orders) {
+      //   this.totalPrice += (parseInt(orderDetail.price) * parseInt(orderDetail.quantity));
+      // }
+    })
   }
-  handdleSearch(name: any){
+  handdleSearch(name: any) {
     const keywork = name.target.value;
-  const search = this.shopService.searchProductList(keywork).then(res => {
-    this.products = res;
-  })
+    const search = this.shopService.searchProductList(keywork).then(res => {
+      this.products = res;
+      console.log(res);
+
+    })
   }
-  reloadCurrentPage(id:any){
-    this._Router.navigate(['/product-detail/'+id]);
+  reloadCurrentPage(id: any) {
+    this._Router.navigate(['/product-detail/' + id]);
+  }
+  deleteCart(id: number) {
+    this.ShopService.deleteCart(id).subscribe(res => {
+      this.ShopService.getListOrder(this.customer_id).subscribe(res => {
+        this.orders = res;
+      })
+    })
   }
 
 
--- src/app/shop/components/home.component.ts
+++ src/app/shop/components/home.component.ts
@@ -8,23 +8,28 @@ import { ShopService } from '../shop.service';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private shopService : ShopService) { }
+  constructor(private shopService: ShopService) { }
   url: string = environment.url;
-  trending_top:any[] =[];
-  product_New:any[] = []
+  trending_top: any[] = [];
+  product_New: any[] = []
   ngOnInit() {
     this.trending();
     this.productNew();
   }
-  trending(){
-    this.shopService.tranding_top().subscribe(res =>{
+  trending() {
+    this.shopService.tranding_top().subscribe(res => {
       this.trending_top = res;
     })
   }
 
-  productNew(){
-    this.shopService.productnew().subscribe(res =>{
+  productNew() {
+    this.shopService.productnew().subscribe(res => {
       this.product_New = res;
+    })
+  }
+  addToCart(id: number) {
+    this.shopService.addToCart(id).subscribe(res => {
+      alert('Thêm vào giỏ hàng thành công!');
     })
   }
 
